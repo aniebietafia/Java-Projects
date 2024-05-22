1773f25a80110b6ca321b76d7605d5f9e8bdd4bc
aniebietafia <aniebietafia87@gmail.com>
2024-05-22 11:48:03 +0100

    learning primitive data types in java

---
+++ TestApp/src/main/java/basics/DataTypes.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte numberOfEnglishLetters = 26;
+        System.out.println(numberOfEnglishLetters);
+
+        int milesToSun = 92960000;
+        System.out.println(milesToSun);
+
+        float tempCelsius;
+        double jumpingHeight = 88.3;
+
+        tempCelsius = (float) 25.78;
+        System.out.println(tempCelsius);
+
+    }
+}
