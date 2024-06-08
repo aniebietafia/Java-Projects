1c1872b92ad852c3f4e8ce41389aa1d95cac7076
aniebietafia <aniebietafia87@gmail.com>
2024-06-08 20:55:17 +0100

    using block statements

--- CalcEngine/src/main/java/com/app/calcengine/Main.java
+++ CalcEngine/src/main/java/com/app/calcengine/Main.java
@@ -3,7 +3,7 @@ package com.app.calcengine;
 public class Main {
     public static void main(String[] args) {
         double value1 = 100.0d;
-        double value2 = 50.0d;
+        double value2 = 0.0d;
         double result = 0.0d;
         char opCode = 'd';
 
@@ -13,10 +13,14 @@ public class Main {
             result = value1 - value2;
         else if (opCode == 'm')
             result = value1 * value2;
-        else if (opCode == 'd')
-            result = value1 / value2;
-        else
+        else if (opCode == 'd') {
+            if (value2 != 0)
+                result = value1 / value2;
+        }
+        else {
+            System.out.println("Invalid opCode " + opCode);
             result = 0.0d;
+        }
 
         System.out.println(result);
     }
